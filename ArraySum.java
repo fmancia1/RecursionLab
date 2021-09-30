c13bced176a052138ad5a772ad55fdd1a7d0b6c0
fmancia1 <71108822+fmancia1@users.noreply.github.com>
2021-09-29 22:59:49 -0400

    Add files via upload

---
+++ ArraySum.java
@@ -0,0 +1,8 @@
+class ArraySum {
+    public int sumOfArray(Integer[] a, int index) {
+        if (index > 0)
+            return sumOfArray(a, index - 1) + a[index];
+        else
+            return a[index];
+    }
+}
